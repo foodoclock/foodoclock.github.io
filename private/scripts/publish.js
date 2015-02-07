823c709a2e268521a9b66ea1fed3670e5b0466c5
Samuel Molinari <samuel@molinari.me>
2015-02-07 12:06:45 +0000

    Add console logs

--- private/scripts/publish.js
+++ private/scripts/publish.js
@@ -1,6 +1,13 @@
 var CI = require('../lib/CI');
 
+console.log('Checking if this change need to be publish');
+
 // Only pulish is CI has the right environment to do so
 if(CI.isReadyForPublication()) {
+  console.log('Ready for publication');
   CI.mergePreReleaseToMaster();
+} else {
+  console.log('These changes don\'t need to be published');
 }
+
+console.log('End of the publish script');
